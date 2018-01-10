722d25705021906e69fb5509a840cb47e6ec8608
Amit Saha <amitsaha.in@gmail.com>
2018-01-09 23:40:10 -0500

    update

--- setup.sh
+++ setup.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-ansible-playbook -i "localhost,"  -c local setup.yml --ask-sudo-pass "$@"
+
+PACKAGE_NAME="ansible"
+
+YUM_CMD=$(which dnf)
+APT_GET_CMD=$(which apt-get)
+BREW_CMD=$(which brew)
+
+
+if [[ ! -z $YUM_CMD ]]; then
+   sudo dnf -y install $PACKAGE_NAME
+elif [[ ! -z $APT_GET_CMD ]]; then
+   sudo apt-get -y $PACKAGE_NAME
+elif [[ ! -z $OTHER_CMD ]]; then
+   brew install $PACKAGE_NAME
+else
+   echo "error can't install package $PACKAGE"
+   exit 1;
+fi
+ansible-playbook -i "localhost,"  -c local setup.yml --ask-become-pass "$@"
